05ddc3267d1161867519dabf75091021b9e4d024
LukeFalcox <101217187+LukeFalcox@users.noreply.github.com>
2025-05-12 20:32:53 -0300

    falcod(dev): Criação de Pedido & Produto WI[#0003]

---
+++ Projetos/Pessoais/Desafios/sistemaPagamento/src/main/java/com/devfalco/sistemaPagamento/model/Pedido.java
@@ -0,0 +1,33 @@
+package com.devfalco.sistemaPagamento.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "pedido")
+public class Pedido {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+UUID id;
+String nomecliente;
+String cpf;
+String valorTotal;  
+@OneToMany(cascade = CascadeType.ALL)
+List<Produto> produtos;
+}
---
+++ Projetos/Pessoais/Desafios/sistemaPagamento/src/main/java/com/devfalco/sistemaPagamento/model/Produto.java
@@ -0,0 +1,30 @@
+package com.devfalco.sistemaPagamento.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "produto")
+public class Produto {
+  @GeneratedValue
+  @Id
+UUID id;
+String nomeproduto;
+Integer quantidade;
+String valor;
+UUID pedidoId;
+}
